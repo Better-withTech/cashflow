466b65aa1a8f2f00777bf0131f21c359acfbeff9
Better-withTech <sheila.hutchinson@flatironschool.com>
2019-11-27 23:08:09 -0500

    added team

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -27,11 +27,8 @@ const realEstates = [
 const smallDeals = stocks.concat(realEstates);
 console.log('Small Deals:', smallDeals);
 
-<<<<<<< HEAD
 function startGame() {
     alert('Sheila made this cute little button! Good Luck!!');
 }
-=======
 const ratRace = new RatRace();
 console.log(ratRace);
->>>>>>> b80e885d345857eba226bb54582d123315cfc785

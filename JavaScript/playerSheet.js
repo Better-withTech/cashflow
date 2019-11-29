e24da497858c830524fc4e6880b692e53ccd1a00
BachNewton <kyle1235@gmail.com>
2019-11-28 22:02:47 -0500

    profession is automatically filled in

--- JavaScript/playerSheet.js
+++ JavaScript/playerSheet.js
@@ -1,1 +1,37 @@
 console.log('Player Sheet');
+
+function updateUI() {
+    var moneyElements = document.getElementsByClassName('money');
+
+    for (var element of moneyElements) {
+        var text = element.innerText.substring(1);
+        text = '$' + parseInt(text).toLocaleString();
+        element.innerText = text;
+    }
+}
+
+function getProfession() {
+    var button = document.getElementById('professionButton');
+    button.style.display = 'none';
+
+    var profession = PROFESSIONS[Math.floor(Math.random() * PROFESSIONS.length)];
+
+    document.getElementById('professionTitle').innerText = profession.title;
+    document.getElementById('savings').innerText = profession.savings;
+    document.getElementById('salary').innerText = profession.salary;
+    document.getElementById('perChildExpense').innerText = profession.perChildExpense;
+    document.getElementById('taxExpense').innerText = profession.expenses.tax;
+    document.getElementById('housingExpense').innerText = profession.expenses.housing;
+    document.getElementById('schoolExpense').innerText = profession.expenses.school;
+    document.getElementById('carExpense').innerText = profession.expenses.car;
+    document.getElementById('creditCardExpense').innerText = profession.expenses.creditCard;
+    document.getElementById('retailExpense').innerText = profession.expenses.retail;
+    document.getElementById('otherExpense').innerText = profession.expenses.other;
+    document.getElementById('housingLiability').innerText = profession.liabilities.housing;
+    document.getElementById('schoolLiability').innerText = profession.liabilities.school;
+    document.getElementById('carLiability').innerText = profession.liabilities.car;
+    document.getElementById('creditCardLiability').innerText = profession.liabilities.creditCard;
+    document.getElementById('retailLiability').innerText = profession.liabilities.retail;
+
+    updateUI();
+}
---
+++ JavaScript/professions.js
@@ -0,0 +1,24 @@
+const PROFESSIONS = [
+    {
+        title: 'Engineer',
+        salary: 4900,
+        savings: 400,
+        perChildExpense: 250,
+        expenses: {
+            tax: 1050,
+            housing: 700,
+            school: 60,
+            car: 140,
+            creditCard: 120,
+            retail: 50,
+            other: 1090
+        },
+        liabilities: {
+            housing: 75000,
+            school: 12000,
+            car: 7000,
+            creditCard: 4000,
+            retail: 1000
+        }
+    }
+];

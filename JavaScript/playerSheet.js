2b8e9f50843c7c1f51cb1338e7058061c4b4615f
BachNewton <kyle1235@gmail.com>
2019-11-28 22:26:23 -0500

    Basic calculated fields are updated

--- JavaScript/playerSheet.js
+++ JavaScript/playerSheet.js
@@ -1,6 +1,25 @@
 console.log('Player Sheet');
 
-function updateUI() {
+function getAmount(id) {
+    var text = document.getElementById(id).innerText;
+    text = text.substring(1);
+
+    return parseInt(text);
+}
+
+function recalculate() {
+    var totalExpenses = getAmount('taxExpense') + getAmount('housingExpense') + getAmount('schoolExpense') + getAmount('carExpense') + getAmount('creditCardExpense') + getAmount('retailExpense') + getAmount('otherExpense') + getAmount('childExpense') + getAmount('bankLoanExpense');
+    var passiveIncome = getAmount('interest') + getAmount('dividends') + getAmount('realEstateIncome') + getAmount('businessesIncome');
+    var totalIncome = passiveIncome + getAmount('salary');
+    var cashflow = totalIncome - totalExpenses;
+
+    document.getElementById('totalExpenses').innerText = '$' + totalExpenses;
+    document.getElementById('passiveIncome').innerText = '$' + passiveIncome;
+    document.getElementById('totalIncome').innerText = '$' + totalIncome;
+    document.getElementById('cashflow').innerText = '$' + cashflow;
+}
+
+function formatUI() {
     var moneyElements = document.getElementsByClassName('money');
 
     for (var element of moneyElements) {
@@ -17,21 +36,22 @@ function getProfession() {
     var profession = PROFESSIONS[Math.floor(Math.random() * PROFESSIONS.length)];
 
     document.getElementById('professionTitle').innerText = profession.title;
-    document.getElementById('savings').innerText = profession.savings;
-    document.getElementById('salary').innerText = profession.salary;
-    document.getElementById('perChildExpense').innerText = profession.perChildExpense;
-    document.getElementById('taxExpense').innerText = profession.expenses.tax;
-    document.getElementById('housingExpense').innerText = profession.expenses.housing;
-    document.getElementById('schoolExpense').innerText = profession.expenses.school;
-    document.getElementById('carExpense').innerText = profession.expenses.car;
-    document.getElementById('creditCardExpense').innerText = profession.expenses.creditCard;
-    document.getElementById('retailExpense').innerText = profession.expenses.retail;
-    document.getElementById('otherExpense').innerText = profession.expenses.other;
-    document.getElementById('housingLiability').innerText = profession.liabilities.housing;
-    document.getElementById('schoolLiability').innerText = profession.liabilities.school;
-    document.getElementById('carLiability').innerText = profession.liabilities.car;
-    document.getElementById('creditCardLiability').innerText = profession.liabilities.creditCard;
-    document.getElementById('retailLiability').innerText = profession.liabilities.retail;
+    document.getElementById('savings').innerText = '$' + profession.savings;
+    document.getElementById('salary').innerText = '$' + profession.salary;
+    document.getElementById('perChildExpense').innerText = '$' + profession.perChildExpense;
+    document.getElementById('taxExpense').innerText = '$' + profession.expenses.tax;
+    document.getElementById('housingExpense').innerText = '$' + profession.expenses.housing;
+    document.getElementById('schoolExpense').innerText = '$' + profession.expenses.school;
+    document.getElementById('carExpense').innerText = '$' + profession.expenses.car;
+    document.getElementById('creditCardExpense').innerText = '$' + profession.expenses.creditCard;
+    document.getElementById('retailExpense').innerText = '$' + profession.expenses.retail;
+    document.getElementById('otherExpense').innerText = '$' + profession.expenses.other;
+    document.getElementById('housingLiability').innerText = '$' + profession.liabilities.housing;
+    document.getElementById('schoolLiability').innerText = '$' + profession.liabilities.school;
+    document.getElementById('carLiability').innerText = '$' + profession.liabilities.car;
+    document.getElementById('creditCardLiability').innerText = '$' + profession.liabilities.creditCard;
+    document.getElementById('retailLiability').innerText = '$' + profession.liabilities.retail;
 
-    updateUI();
+    recalculate();
+    formatUI();
 }

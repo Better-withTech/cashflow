ddae5b9ce21e5781436a845361526751eef4a655
BachNewton <kyle1235@gmail.com>
2019-11-29 18:52:46 -0500

    Added more professions

--- JavaScript/professions.js
+++ JavaScript/professions.js
@@ -20,5 +20,93 @@ const PROFESSIONS = [
             creditCard: 4000,
             retail: 1000
         }
+    },
+    {
+        title: 'Airline Pilot',
+        salary: 9500,
+        savings: 400,
+        perChildExpense: 480,
+        expenses: {
+            tax: 2350,
+            housing: 1330,
+            school: 0,
+            car: 300,
+            creditCard: 660,
+            retail: 50,
+            other: 2210
+        },
+        liabilities: {
+            housing: 143000,
+            school: 0,
+            car: 15000,
+            creditCard: 22000,
+            retail: 1000
+        }
+    },
+    {
+        title: 'Mechanic',
+        salary: 2000,
+        savings: 670,
+        perChildExpense: 110,
+        expenses: {
+            tax: 360,
+            housing: 300,
+            school: 0,
+            car: 60,
+            creditCard: 60,
+            retail: 50,
+            other: 450
+        },
+        liabilities: {
+            housing: 31000,
+            school: 0,
+            car: 3000,
+            creditCard: 2000,
+            retail: 1000
+        }
+    },
+    {
+        title: 'Janitor',
+        salary: 1600,
+        savings: 560,
+        perChildExpense: 70,
+        expenses: {
+            tax: 280,
+            housing: 200,
+            school: 0,
+            car: 60,
+            creditCard: 60,
+            retail: 50,
+            other: 300
+        },
+        liabilities: {
+            housing: 20000,
+            school: 0,
+            car: 4000,
+            creditCard: 2000,
+            retail: 1000
+        }
+    },
+    {
+        title: 'Nurse',
+        salary: 3100,
+        savings: 480,
+        perChildExpense: 170,
+        expenses: {
+            tax: 600,
+            housing: 400,
+            school: 30,
+            car: 100,
+            creditCard: 90,
+            retail: 50,
+            other: 710
+        },
+        liabilities: {
+            housing: 47000,
+            school: 6000,
+            car: 5000,
+            creditCard: 3000,
+            retail: 1000
+        }
     }
 ];
